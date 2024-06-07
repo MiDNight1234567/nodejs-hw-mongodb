b6b89da1585f3a8cb58db73a31612e3db9f8ad94
Данил <danikkach666@gmail.com>
2024-06-07 19:20:48 +0300

    add

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -4,8 +4,8 @@ export const createContactSchema = Joi.object({
   name: Joi.string().min(3).max(20).required(),
   phoneNumber: Joi.string().min(3).max(20).required(),
   email: Joi.string().email(),
-  isFavourite: Joi.boolean().required(),
-  contactType: Joi.string().valid('work', 'home', 'personal').required(),
+  isFavourite: Joi.boolean(),
+  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
 
 export const updateContactSchema = Joi.object({
