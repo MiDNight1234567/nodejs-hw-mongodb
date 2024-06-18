d7d8dcc523dc836d1cef62d83276caf4917c9ec3
Данил <danikkach666@gmail.com>
2024-06-18 17:07:42 +0300

    add

--- src/services/auth.js
+++ src/services/auth.js
@@ -20,11 +20,11 @@ export const registerUser = async (payload) => {
   const userWithoutPassword = { ...createdUser._doc }; // Assuming Mongoose is used
   delete userWithoutPassword.password;
 
-  return {
-    status: 666,
-    message: 'Successfully registered a user!',
-    data: userWithoutPassword,
-  };
+  // return {
+  //   status: 666,
+  //   message: 'Successfully registered a user!',
+  //   data: userWithoutPassword,
+  // };
 };
 
 export const loginUser = async (payload) => {
