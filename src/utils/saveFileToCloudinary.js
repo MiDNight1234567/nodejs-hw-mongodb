79326270b2e25e165c4a8bf44e12832e9f8387bc
Данил <danikkach666@gmail.com>
2024-06-22 12:15:32 +0300

    add

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -5,8 +5,8 @@ import { CLOUDINARY } from '../constants/index.js';
 cloudinary.v2.config({
   secure: true,
   cloud_name: env(CLOUDINARY.CLOUD_NAME),
-  api_key: env(CLOUDINARY.API_KEY),
-  api_secret: env(CLOUDINARY.API_SECRET),
+  api_key: env(CLOUDINARY.CLOUD_API_KEY),
+  api_secret: env(CLOUDINARY.CLOUD_API_SECRET),
 });
 
 export const saveFileToCloudinary = async (file) => {
