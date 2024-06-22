07b19750827e56cf85da06c7be2ddbf0c221c656
Данил <danikkach666@gmail.com>
2024-06-22 12:06:58 +0300

    add

--- src/constants/index.js
+++ src/constants/index.js
@@ -31,6 +31,6 @@ export const TEMP_UPLOAD_DIR = path.join(process.cwd(), 'temp');
 
 export const CLOUDINARY = {
   CLOUD_NAME: 'CLOUD_NAME',
-  API_KEY: 'API_KEY',
-  API_SECRET: 'API_SECRET',
+  CLOUD_API_KEY: 'API_KEY',
+  CLOUD_API_SECRET: 'API_SECRET',
 };
